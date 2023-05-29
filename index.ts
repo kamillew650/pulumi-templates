ab2998248786e1c3cc3f8ec37a8e09d1deb67511
kamil.lewandowski <kamil.lewandowski1324@gmail.com>
2023-05-29 20:27:05 +0200

    refactor: change parameter name and value

--- index.ts
+++ index.ts
@@ -3,11 +3,12 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 const sssParameter = new aws.ssm.Parameter("my-ssm-parameter", {
-  type: "SecureString",
-  value: "my-ssm-parameter-value",
+  name: "my-test-parameter",
+  type: "String",
+  value: "to-jest-test",
 });
 
-const devAppPath = "dev-app";
+const devAppPath = "dev-my-app";
 
 const appParam1 = new aws.ssm.Parameter("dev-app-login", {
   name: `/${devAppPath}/LOGIN`,
@@ -19,4 +20,5 @@ const appParam2 = new aws.ssm.Parameter("dev-app-password", {
   name: `/${devAppPath}/PASSWORD`,
   type: "SecureString",
   value: `password`,
+  keyId: "569aa8e4-21b9-4357-a591-cdb145680a79",
 });
